3747279a939f7f1565bf8eed448daef5de788845
AdrianRoger <114889467+AdrianRoger@users.noreply.github.com>
2024-01-09 23:27:47 -0300

    [feat]: create page Reserva

---
+++ pages/Reserva.js
@@ -0,0 +1,62 @@
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export default function Reserva(){
+
+  const [reservas, setReservas] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let response = await axios.get('https://localhost:7244/api/Reserva');
+        setReservas(response.data);
+        console.log(response);
+      } catch (error) {
+        console.error('Erro ao buscar na base de dados', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <section>
+			<div className="container max-formulario py-5">
+				<div className="row">
+					<div className="col">
+						<h1 className="text-center titulo-3">Reservas</h1>
+						<hr className="group-divider"></hr>
+						<div className="table-responsive">
+							<table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Data reserva</th>
+                    <th>Qtd pessoas</th>
+                    <th>Cliente</th>
+                    <th>Preço total</th>
+                    <th>Origem</th>
+                    <th>Destino</th>
+                    <th>Cancelada</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {reservas.map((r) => (
+                    <tr key={r.id}>
+                      <td>{r.dataReserva}</td>	
+                      <td>{r.numPessoas}</td>
+                      <td>{r.usuario.nome}-{r.usuario.cpf}</td>
+                      <td>R$ {r.voo.precoUnit * r.numPessoas}</td>
+                      <td>{r.voo.origem.nomeCidade}-{r.voo.origem.estado}</td>
+                      <td>{r.voo.destino.nomeCidade}-{r.voo.destino.estado}</td>
+                      <td>{r.cancelada ? 'Sim' : 'Não'}</td>
+                    </tr>
+                  ))}
+                </tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+  );
+}
